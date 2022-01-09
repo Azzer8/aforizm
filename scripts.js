a6ea9eb139a627ff7ab3cba6e468461a8901f441
Azzer8 <47861195+Azzer8@users.noreply.github.com>
2022-01-09 13:32:20 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -18,3 +18,16 @@ function Lang(evt, lang) {
     document.getElementById(lang).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
